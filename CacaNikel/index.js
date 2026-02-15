68127c43c3f02c2014b4b27ede4684e4b8821336
DavidTelles <167997595+DavidTelles@users.noreply.github.com>
2026-02-15 17:40:56 -0300

    upgrade the sistem

--- CacaNikel/index.js
+++ CacaNikel/index.js
@@ -1,7 +1,9 @@
 const prompt = require ('prompt-sync')();
 
+let saldo = Number(prompt('Digite seu Saldo -> '))
+
 function girar() {
-    const simbolos = ['7', '%', '#', '$', '#', '@']
+    const simbolos = ['7', '%', '#', '$', '@']
     let res = []
 
     for (let i = 0; i < 3; i++) {
@@ -12,18 +14,42 @@ function girar() {
     return res
 }
 
-function verficar(res) {
+function verficar(res, aposta) {
     if(res[0] === res[1] && res[0] === res[2]){
-        return '🎉 JACKPOT!!'
-    } else {
-        return 'Lost!'
+        if(res[0] == '7') {return aposta * 10}
+        else if(res[0] == '$') {return aposta * 7}
+        else {return aposta * 5}
     }
+
+    return 0
 }
 
 function game() {
+    let apostado = Number(prompt('Quanto quer apostar? -> '))
+
+    if(apostado > saldo){
+        console.log('Aposta invalida!! X')
+        return;
+    }
+
+    saldo -= apostado
+
     const res = girar()
+    console.log('\n Girando...')
     console.log(res.join('|'))
-    console.log(verficar(res))
+    const ganho = verficar(res, apostado)
+
+    if(ganho > 0) {
+        console.log(`JAKEPOT! Você ganhou: ${ganho}`)
+        saldo += ganho;
+    } else {
+        console.log('Perdeu...')
+    }
+
+    if (saldo <= 0) {
+        console.log('\n Você faliu no cassino!')
+        process.exit()
+    }
 }
 
 while(true) {
